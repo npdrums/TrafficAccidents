9bca3644b4537af770d59e547cb468b06d188084
npdrums <nikola.pujaz@gmail.com>
2023-08-09 23:08:22 +0200

    Add Municipality Data Model

---
+++ Infrastructure/Database/Configuration/MunicipalityEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Database.Entities.Enums;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configuration;
+
+public class MunicipalityEntityTypeConfiguration : IEntityTypeConfiguration<MunicipalityDataModel>
+{
+    public void Configure(EntityTypeBuilder<MunicipalityDataModel> builder)
+    {
+        builder.ToTable("municipalities");
+
+        builder.HasKey(x => x.MunicipalityId);
+
+        builder.Property(x => x.MunicipalityName).HasMaxLength(150);
+        builder.Property(x => x.MunicipalityBorder).HasColumnType("geometry(geometry, 4326)");
+    }
+}
---
+++ Infrastructure/Database/Entities/Enums/MunicipalityDataModel.cs
@@ -0,0 +1,12 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace Infrastructure.Database.Entities.Enums;
+
+public class MunicipalityDataModel
+{
+    public Guid MunicipalityId { get; set; }
+
+    public string MunicipalityName { get; set; } = null!;
+
+    public Geometry MunicipalityBorder { get; set; } = null!;
+}
